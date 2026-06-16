b0d5ecf7b53585e5e73105e09cc50bca7e7ec300
amir-hdri <168375652+amir-hdri@users.noreply.github.com>
2026-06-16 11:28:05 +0000

    🔒 Fix hardcoded CORS origin

--- robot trader/server/index.js
+++ robot trader/server/index.js
@@ -78,7 +78,7 @@ const app = express();
 app.use(apiMetrics());
 const port = 3000;
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use((req, res, next) => {
